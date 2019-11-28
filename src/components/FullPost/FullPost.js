22fdfcee08115468dc040f1379d7bbb606f5ee51
Vladimir Pavković <vladimirpavk@gmail.com>
2019-11-28 13:00:29 +0100

    +

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 import './FullPost.css';
 
 class FullPost extends Component {
+
+    constructor(props){
+        super(props);        
+    }
+
     render () {
         let post = <p>Please select a Post!</p>;
         post = (
             <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
+                <h1>{this.props.title}</h1>
+                <p>{this.props.content}</p>
                 <div className="Edit">
                     <button className="Delete">Delete</button>
                 </div>
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,17 +5,90 @@ import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
+import axios from 'axios';
+
 class Blog extends Component {
+    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            posts : [],
+            selectedPost : {}
+
+        };
+    }
+
+    componentWillMount(){
+        console.log('Will get data...');
+        this.getData();
+    }    
+
+    getData = ()=>{
+        axios.get('https://jsonplaceholder.typicode.com/posts')            
+            .then(
+                (res)=>
+                {
+                    //console.log(res.data);
+                    const updatedPosts = res.data.slice(0, 4).map(
+                        (post)=>{
+                            return {
+                                ...post,
+                                author: 'Vladimir'
+                            }
+                        }
+                    );
+
+                    this.setState({
+                        posts: updatedPosts
+                    });                    
+                }
+            )
+            .catch(
+                (err)=>
+                    {
+                        alert('Something bad happened...', err);
+                        console.log(err);
+                    }
+            );
+    }
+
+    postSelected = (id)=>{
+        const filteredPost = this.state.posts.filter(
+            (post)=>post.id === id
+        );
+        console.log(...filteredPost);        
+        this.setState({
+            selectedPost : filteredPost[0]
+        })
+    }
+
     render () {
+        const posts = this.state.posts.map(
+            (post)=>{
+                return (
+                    <Post
+                        key={post.id}
+                        id={post.id}
+                        title={post.title}
+                        author={post.author}
+                        postSelected={this.postSelected}
+                        />
+                );
+            }
+        );        
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost 
+                        title={this.state.selectedPost.title}
+                        content={this.state.selectedPost.body}
+                        id={this.state.selectedPost.id}
+                    />
                 </section>
                 <section>
                     <NewPost />
